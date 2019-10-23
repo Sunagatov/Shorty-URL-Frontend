5f65d70247ddcb09b2af0c1d6400e747100319cb
Сунагатов Зуфар Рустамович <sunagatovzr@vdcom.ru>
2019-10-24 00:14:41 +0400

    Initial commit

---
+++ src/main/java/com/zufar/client_service/Application.java
@@ -0,0 +1,15 @@
+package com.zufar.client_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableDiscoveryClient
+@EnableFeignClients
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
