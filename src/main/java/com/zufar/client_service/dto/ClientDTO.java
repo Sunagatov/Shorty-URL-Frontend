13c1150a225ebe84764b3025513525001d0ecca2
Сунагатов Зуфар Рустамович <sunagatovzr@vdcom.ru>
2019-10-24 01:50:26 +0400

    Validation and swagger annotations were added.

--- src/main/java/com/zufar/client_service/dto/ClientDTO.java
+++ src/main/java/com/zufar/client_service/dto/ClientDTO.java
@@ -1,9 +1,14 @@
 package com.zufar.client_service.dto;
 
+import io.swagger.annotations.ApiModelProperty;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
 import java.util.Set;
 
@@ -11,14 +16,42 @@ import java.util.Set;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ClientDTO {
-    
+
+    @ApiModelProperty(notes = "client's id", name="id")
     private Long id;
+
+    @ApiModelProperty(notes = "client's short name", name="shortName", required=true)
+    @NotEmpty(message = "please provide a client's short name. It is empty")
+    @NotNull(message = "please provide a client's short name")
+    @Size(min = 5, max = 60, message = "a client's short name length should be from 5 to 60")
     private String shortName;
+
+    @ApiModelProperty(notes = "client's full name", name="fullName", required=true)
+    @NotEmpty(message = "please provide a client's full name. It is empty")
+    @NotNull(message = "please provide a client's full name")
+    @Size(min = 5, max = 255, message = "a client's short name length should be from 5 to 255")
     private String fullName;
+
+    @ApiModelProperty(notes = "client's type id", name="clientTypeId", required=true)
+    @NotNull(message = "please provide a client's type id")
     private Long clientTypeId;
+
+    @ApiModelProperty(notes = "client's inn", name="inn", required=true)
+    @NotEmpty(message = "please provide a client's inn. It is empty")
+    @NotNull(message = "please provide a client's inn")
+    @Size(min = 5, max = 12, message = "a client's short name length should be from 5 to 12")
     private String inn;
+
+    @ApiModelProperty(notes = "client's okpo", name="okpo")
+    @Size(min = 5, max = 10, message = "a client's short name length should be from 5 to 10")
     private String okpo;
+
+    @ApiModelProperty(notes = "order's ids of a client", name="orderIds")
     private Set<Order> orderIds;
+
+    @ApiModelProperty(notes = "client's creation date", name="creationDate", required=true)
     private LocalDateTime creationDate;
+
+    @ApiModelProperty(notes = "client's modification date", name="modificationDate", required=true)
     private LocalDateTime modificationDate;
 }
--- src/main/java/com/zufar/client_service/entity/ClientType.java
+++ src/main/java/com/zufar/client_service/entity/ClientType.java
@@ -1,5 +1,7 @@
 package com.zufar.client_service.entity;
 
+import io.swagger.annotations.ApiModelProperty;
+
 import lombok.Data;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
@@ -19,18 +21,22 @@ import javax.persistence.GenerationType;
 @Entity
 @Table(name = "types")
 public class ClientType {
-    
+
+    @ApiModelProperty(notes = "client type's id", name="id", required=true)
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "type_sequence")
     @SequenceGenerator(name = "type_sequence", sequenceName = "type_seq")
     private Long id;
 
+    @ApiModelProperty(notes = "client type's short name", name="shortName", required=true)
     @Column(name = "short_name", length = 60, nullable = false)
     private String shortName;
 
+    @ApiModelProperty(notes = "client type's full name", name="fullName", required=true)
     @Column(name = "full_name", length = 60, nullable = false)
     private String fullName;
 
+    @ApiModelProperty(notes = "client type's code", name="typeCode", required=true)
     @Column(name = "client_type_code", length = 10, nullable = false)
     private String typeCode;
 }
