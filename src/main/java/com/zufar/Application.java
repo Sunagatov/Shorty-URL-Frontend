cdf57ce0a51b7bdf918e8df039dc3d26302b01e7
Сунагатов Зуфар Рустамович <sunagatovzr@vdcom.ru>
2019-10-25 17:27:22 +0400

    SwaggerConfig class was added.

--- src/main/java/com/zufar/Application.java
+++ src/main/java/com/zufar/Application.java
@@ -2,42 +2,15 @@ package com.zufar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@EnableSwagger2
 @EnableDiscoveryClient
 @EnableFeignClients
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-    }
-
-    @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
-                .build().apiInfo(apiEndPointsInfo());
-    }
-
-    private ApiInfo apiEndPointsInfo() {
-        return new ApiInfoBuilder()
-                .title("Client REST API")
-                .description("Client REST API provides api for working with clients")
-                .contact(new Contact("Zufar Sunagatov", "https://vk.com/person99", "zufar.sunagatov@gmail.com"))
-                .version("1.0.0")
-                .build();
     }
 }
---
+++ src/main/java/com/zufar/configuration/SwaggerConfig.java
@@ -0,0 +1,36 @@
+package com.zufar.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@Configuration
+public class SwaggerConfig {
+    
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build().apiInfo(apiEndPointsInfo());
+    }
+
+    private ApiInfo apiEndPointsInfo() {
+        return new ApiInfoBuilder()
+                .title("Client REST API")
+                .description("Client REST API provides api for working with clients")
+                .contact(new Contact("Zufar Sunagatov", "https://vk.com/person99", "zufar.sunagatov@gmail.com"))
+                .version("1.0.0")
+                .build();
+    }
+}
