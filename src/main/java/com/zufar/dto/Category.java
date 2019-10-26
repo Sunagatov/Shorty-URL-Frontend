7956a89b148aa6d7ed010fc8f9e416db18f1124b
Сунагатов Зуфар Рустамович <sunagatovzr@vdcom.ru>
2019-10-26 16:00:22 +0400

    Swagger annotations were added.

--- src/main/java/com/zufar/dto/Category.java
+++ src/main/java/com/zufar/dto/Category.java
@@ -1,14 +1,21 @@
 package com.zufar.dto;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
 import lombok.Data;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
+@ApiModel("Category")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Category {
 
+    @ApiModelProperty(notes = "Category id", name = "id", required = true)
     private Long id;
+
+    @ApiModelProperty(notes = "Category name", name = "name", required = true)
     private String name;
 }
--- src/main/java/com/zufar/dto/Order.java
+++ src/main/java/com/zufar/dto/Order.java
@@ -1,16 +1,26 @@
 package com.zufar.dto;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
 
+@ApiModel("Order")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Order {
 
+    @ApiModelProperty(notes = "Order id", name = "id", required = true)
     private Long id;
+
+    @ApiModelProperty(notes = "Order goods name", name = "goodsName", required = true)
     private String goodsName;
+
+    @ApiModelProperty(notes = "Order category", name = "category", required = true)
     private Category category;
+
+    @ApiModelProperty(notes = "Order client id", name = "clientId", required = true)
     private Long clientId;
 }
